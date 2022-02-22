97637e009dcf2ee0b5d50a0af3f9435a1387d021
Michal Kierzynkowski <michu3136@gmail.com>
2022-02-22 12:58:38 +0100

    First Exercise done

--- src/FirstExercise/Program.cs
+++ src/FirstExercise/Program.cs
@@ -1,16 +1,33 @@
-﻿namespace FirstExercise
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FirstExercise
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("First Number:!");
+            int a, b;
+            Console.WriteLine("Podaj wartość a=");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wartość b=");
+            b = int.Parse(Console.ReadLine());
+
+            var result = SwapValues(a, b);
+            
+            Console.WriteLine($"Zamienione wartości: a = {result.Item1} ; b = {result.Item2}");
+            Console.ReadKey();
+
         }
 
-        static int SwapValues(int x, int y)
+        
+
+         public static  Tuple <int,int> SwapValues(int a, int b)
         {
-
-
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            var tuple = new Tuple<int, int>(a, b);
+            return tuple;
 
         }
     }
---
+++ test/Tests/ConsoleAppTests/Tests.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+using FirstExercise;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.ObjectModel;
+
+namespace FirstExercise.Tests
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(5,10)]
+        [InlineData(2,7)]
+        [InlineData(0,4)]
+        [InlineData(-2,4)]
+      
+        public void SwapValuesTest(int a,int b)
+        {
+            int tempA = a;
+            int tempB = b;
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            var tuple = new Tuple<int, int>(a, b);
+            //tuple.Item1.S
+            tuple.Item1.Should().Be(tempB);
+            tuple.Item2.Should().Be(tempA);
+        }
+    }
+}
