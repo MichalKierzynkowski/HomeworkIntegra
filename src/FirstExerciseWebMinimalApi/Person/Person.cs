02823db32018716070937ca33d75b8ad885c3573
michu3136 <71141129+michu3136@users.noreply.github.com>
2022-02-23 16:08:23 +0100

    Add model Person

---
+++ src/FirstExerciseWebMinimalApi/Person/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstExerciseWebMinimalApi.Person
+{
+    public class Person
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int HouseNumber { get; set; }
+        public int PostalCode { get; set; }
+    }
+}
